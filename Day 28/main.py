a3090b9ff0314995ad06b75e383171bbcb6e84ec
madhu <madhukiran2k6@gmail.com>
2025-12-03 00:15:26 +0530

    Password Manager GUI Added

---
+++ Day 28/main.py
@@ -0,0 +1,90 @@
+import math
+import tkinter
+from tkinter import PhotoImage
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_button_clicked():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text,text= "00:00")
+    timerLabel.config(text="Timer")
+    checkButton.config(text="")
+    global reps
+    reps = 0
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_button_clicked():
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    global reps
+
+    reps += 1
+    if reps % 8 == 0:
+        timerLabel.config(text="Long Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timerLabel.config(text="Short Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timerLabel.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = int(count / 60)
+    count_sec = int(count % 60)
+
+    if count_sec < 10:
+        count_sec = "0" + str(count_sec)
+
+    canvas.itemconfig(timer_text,text= f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_button_clicked()
+        final = math.floor(reps / 2)
+        mark = ""
+        for i in range(final):
+            mark += "✔"
+        checkButton.config(text=mark, fg=GREEN)
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+timerLabel = tkinter.Label(text= "Timer",font= (FONT_NAME,40,"bold"),bg=YELLOW,fg= GREEN)
+timerLabel.grid(column=2, row=1)
+
+startButton = tkinter.Button(text="Start",highlightthickness=0,command=start_button_clicked)
+startButton.grid(column=1, row=3)
+
+checkButton = tkinter.Label(font= (FONT_NAME,16,"bold"),background=YELLOW,fg=GREEN)
+checkButton.grid(column=2, row=4)
+
+resetButton = tkinter.Button(text="Reset",highlightthickness=0,command=reset_button_clicked)
+resetButton.grid(column=3, row=3)
+
+
+
+
+tomato_image = tkinter.PhotoImage(file = "tomato.png")
+canvas = tkinter.Canvas(width=230, height=230,bg = YELLOW,highlightthickness=0)
+canvas.create_image(112, 115, image=tomato_image)
+timer_text = canvas.create_text(115,130,text= "00:00",font= (FONT_NAME,30,"bold"),fill= "White")
+canvas.grid(column=2, row=2)
+
+
+
+window.mainloop()
---
+++ Day 29/main.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+from tkinter import messagebox
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8,10)
+nr_symbols = random.randint(2,4)
+nr_numbers = random.randint(2,4)
+
+list_passwords = []
+for i in range(0, nr_letters):
+    list_passwords.append(random.choice(letters))
+
+for i in range(0, nr_symbols):
+    list_passwords.append(random.choice(symbols))
+
+for i in range(0, nr_numbers):
+    list_passwords.append(random.choice(numbers))
+
+print(list_passwords)
+random.shuffle(list_passwords)
+
+final_password = "".join(list_passwords)
+
+def generate_password_button_clicked():
+    pyperclip.copy(final_password)
+    password_entry.insert(0, final_password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_button_clicked():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    if password != "" and website != "":
+        is_ok = messagebox.askokcancel(title=f"{website}",message=f"These are details entered:\n"
+                                                                  f"Email: {email}\nPassword: {password}\nIs it Ok?")
+        if is_ok:
+            with open(file="data.txt", mode="a") as data_file:
+                data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+                website_entry.delete(0, tk.END)
+                password_entry.delete(0, tk.END)
+                website_entry.focus()
+    elif password == "":
+        messagebox.showinfo(title="Password?!", message="Please enter your password")
+    else:
+        messagebox.showinfo(title="Oops?!", message="Please enter the details")
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title("Password Manager")
+window.minsize(300, 300)
+window.config(padx=20, pady=20)
+
+website_text = tk.Label(text="Website:",font=("Georgia",9,"normal"))
+website_text.grid(row=1,column=0)
+
+website_entry = tk.Entry(width=53)
+website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
+
+email_text = tk.Label(text="Email/Username:",font=("Georgia",9,"normal"))
+email_text.grid(row=2,column=0)
+
+email_entry = tk.Entry(width=53)
+email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,"madhukiran2k6@gmail.com")
+
+password_text = tk.Label(text="Password:",font=("Georgia",9,"normal"))
+password_text.grid(row=3,column=0)
+
+password_entry = tk.Entry(width=33)
+password_entry.grid(row=3,column=1)
+
+generatePassword = tk.Button(text="Generate Password",highlightthickness=0,font=("Georgia",9,"normal"),
+                             width=14,command=generate_password_button_clicked)
+generatePassword.grid(row=3,column=2)
+
+add_button = tk.Button(text="Add",width=40,highlightthickness=0,font=("Georgia",9,"normal"),command=add_button_clicked)
+add_button.grid(row=4,column=1,columnspan=2)
+
+password_img = tk.PhotoImage(file="logo.png")
+canvas = tk.Canvas(width=200, height=200)
+canvas.create_image(100, 105, image=password_img)
+canvas.grid(row=0, column=1)
+
+window.mainloop()
