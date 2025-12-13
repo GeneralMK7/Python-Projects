1858f0e976237434125b62be71bd1f0bc864bc07
madhu <madhukiran2k6@gmail.com>
2025-12-14 01:18:49 +0530

    changed url

--- Stock Alert Project/main.py
+++ Stock Alert Project/main.py
@@ -7,7 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
-## STEP 1: Use https://newsapi.org/docs/endpoints/everything
+## STEP 1: Use "https://www.alphavantage.co/query"
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 #HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
 #HINT 2: Work out the value of 5% of yesterday's closing stock price.
